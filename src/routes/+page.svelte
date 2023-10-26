54731926efed254f8acecf40b3b50ea1655b70f9
BenoitRobin <benoitrobin@me.com>
2023-10-26 12:00:23 +0200

    feat: :sparkles: image-gallery interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
-    import Calculator from "./calculator.svelte";
+	import Calculator from './calculator.svelte';
+	import Gallery from './gallery.svelte';
 </script>
 
-<Calculator />
+
+
+<!-- <Calculator /> -->
+<Gallery />
---
+++ src/routes/navbar.svelte
@@ -0,0 +1,14 @@
+<div class="grid grid-cols-6 h-24 items-center px-5 bg-gray-500 text-white text-lg ">
+	<div class="">
+		<h2>Benoit Robin</h2>
+	</div>
+	<nav class=" flex list-none text-center col-span-4">
+		<li class="w-1/4"><a href="/">Home</a></li>
+       	<li class="w-1/4"><a href="/">Calculator</a></li>
+		<li class="w-1/4"> <a href="/">Galery</a></li>
+		<li class="w-1/4"><a href="/">Quizz</a></li>
+	</nav>
+	<div class="">
+		<a href="/">picture</a>
+	</div>
+</div>
