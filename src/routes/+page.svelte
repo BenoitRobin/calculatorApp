186666bde16c0c5ddd1b8493e19694e52208f2bb
BenoitRobin <benoitrobin@me.com>
2023-10-27 13:59:22 +0200

    feat: :sparkles: quizz init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Calculator from './calculator.svelte';
 	import Gallery from './gallery.svelte';
+	import Quizz from './quizz/+page.svelte'
 </script>
 
 
 
+<!-- <Gallery /> -->
 <!-- <Calculator /> -->
-<Gallery />
+<Quizz />
---
+++ src/routes/quizz/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import informatique from '../../lib/informatique.svg';
+	import television from '../../lib/television.svg';
+</script>
+
+<div class="quizzes-container w-[700px] m-auto flex justify-center h-screen items-center">
+	<div class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer bg-slate-300">
+		<img src={informatique} alt="" class="h-44 m-auto" />
+		<div class="flex items-center justify-between py-3 px-2">
+			<h1 class="font-bold">Informatique</h1>
+			<p>5 questions</p>
+		</div>
+	</div>
+</div>
